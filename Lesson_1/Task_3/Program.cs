56a77d2de510d5d227f13fa2d8eaf7587a20d71f
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-11-29 07:23:24 +0500

    Ending Task_3 - Working right

---
+++ Lesson_1/Task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+int numA, numB, result1;
+double numC, numD, result0;
+numA = new Random().Next(1, 10);
+numB = new Random().Next(1, 10);
+numC = numA;
+numD = numB;
+
+Console.Write("Значение первого числа: ");
+Console.WriteLine(numA);
+Console.Write("Значение второго числа: ");
+Console.WriteLine(numB);
+
+result1 = numA+numB;
+Console.WriteLine("Сумма "+numA+" и "+numB+" равняется: "+result1);
+
+result1 = numA*numB;
+Console.WriteLine("Произведение "+numA+" и "+numB+" равняется: "+result1);
+
+result1 = numA-numB;
+Console.WriteLine("Вычитание "+numB+" из "+numA+" равняется: "+result1);
+
+result0 = numC/numD;
+Console.WriteLine("Деление "+numB+" на "+numA+" равняется: "+result0);
