31bdfc59ec5ab63fe5af1629ebb69a91d794e3a4
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-11-30 23:21:14 +0500

    Аdd task 3 of practice - working right

---
+++ Lesson_1_prac/Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int numMain = Convert.ToInt32(Console.ReadLine());
+
+int sotni, edin, sum1and3;
+
+sotni = numMain/100;
+edin = numMain%10;
+
+sum1and3 = sotni + edin;
+Console.WriteLine("Сумма первой и третьей цифры числа сверху: "
++sum1and3);
