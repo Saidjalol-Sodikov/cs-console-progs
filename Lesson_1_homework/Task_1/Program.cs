035aa450a9104a62e0efc924bd99eb5ad7c7fbf1
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-01 00:19:23 +0500

    Аdd task 3 of homework - working right and try 1 time

---
+++ Lesson_1_homework/Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿int firstNumber;
+int secondNumber;
+
+firstNumber = new Random().Next(1, 100);
+secondNumber = new Random().Next(1, 100);
+
+if (firstNumber==secondNumber) {
+    Console.WriteLine($"Введенные числа равны `{firstNumber}`");
+} else {
+    if (firstNumber<secondNumber) {
+        Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
+    } else {
+        Console.WriteLine($"Первое число `{secondNumber}` меньше чем второе число `{firstNumber}`");
+    };
+};
