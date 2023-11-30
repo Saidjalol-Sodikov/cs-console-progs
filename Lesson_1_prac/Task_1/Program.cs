2be5ec8b781e90074101bfe4247874828f0e0cf4
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-11-30 22:38:06 +0500

    Аdd task 1 of practice - working right

---
+++ Lesson_1_prac/Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿//Программа проверки квадрата первого входящего числа со вторым
+//числом.
+
+Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int bSquar = b*b;
+
+if (bSquar == a) {
+    Console.Write("Да");
+} else {
+    Console.Write("Нет");
+}
