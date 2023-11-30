befde3ae5aadfda4f19028b14675310d7e31bf93
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-11-30 22:48:36 +0500

    Аdd task 2 of practice - working right

---
+++ Lesson_1_prac/Task_2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число : ");
+int numNpos = Convert.ToInt32(Console.ReadLine());
+
+int numNneg = numNpos*(-1);
+
+while (numNneg <= numNpos) {
+    Console.Write(numNneg+" ");
+    numNneg ++;
+};
