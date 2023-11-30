e9cecd0eb54f157fda65e7375dc15916f481e06f
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-01 01:00:11 +0500

    Аdd task 2 of homework - working right and no try

--- Lesson_1_homework/Task_2/Program.cs
+++ Lesson_1_homework/Task_2/Program.cs
@@ -1,9 +1,12 @@
-﻿int a, b, c, max;
+﻿int a, b, c;
 
 a = new Random().Next(1, 100);
 b = new Random().Next(1, 100);
 c = new Random().Next(1, 100);
 
+//То, что ниже можно скопировать и вставить
+int max = a;
+if (max < b) max = b;
+if (max < c) max = c;
 
-
-Console.WriteLine("Hello, World!");
+Console.WriteLine(max);
