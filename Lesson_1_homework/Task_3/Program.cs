48aa410e8f2e6053775d2eff4bfbc54ba5effc61
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-01 01:09:32 +0500

    Аdd task 3 of homework - working right and try 1 time

---
+++ Lesson_1_homework/Task_3/Program.cs
@@ -0,0 +1,8 @@
+﻿int number = new Random().Next(1, 100);
+
+////То, что ниже можно скопировать и вставить
+if (number%2 == 0) {
+    Console.WriteLine($"Число `{number}` чётное");
+} else {
+    Console.WriteLine($"Число `{number}` нечётное");
+}
