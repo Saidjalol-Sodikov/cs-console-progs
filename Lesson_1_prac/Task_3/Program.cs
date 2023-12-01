8d526614d0e10c19cff1ca689ceed0861cf3a509
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-02 01:53:54 +0500

    Modefied task 3 of practice - working right and infinity

--- Lesson_1_prac/Task_3/Program.cs
+++ Lesson_1_prac/Task_3/Program.cs
@@ -1,6 +1,8 @@
-﻿Console.Write("Введите трёхзначное число: ");
+﻿bool term = true;
+while (term) {
+Console.Write("Введите трёхзначное число: ");
 int numMain = Convert.ToInt32(Console.ReadLine());
-
+if (numMain==0) term = false;
 int sotni, edin, sum1and3;
 
 sotni = numMain/100;
@@ -9,3 +11,4 @@ edin = numMain%10;
 sum1and3 = sotni + edin;
 Console.WriteLine("Сумма первой и третьей цифры числа сверху: "
 +sum1and3);
+}
