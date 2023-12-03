b80582fffa17f9875b07ee709b88bffbb92f062b
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-04 02:08:45 +0500

    Мodified 2-lesson's 1-task - working right

--- Lesson_2_prac/Task_1/Program.cs
+++ Lesson_2_prac/Task_1/Program.cs
@@ -1,7 +1,5 @@
-﻿while (true) {
-Console.Write("Введите трёхзначное число: ");
+﻿Console.Write("Введите трёхзначное число: ");
 int numMain = Convert.ToInt32(Console.ReadLine());
-if (numMain==0) break;
 int sotni, edin, result;
 
 sotni = numMain/100;
@@ -9,4 +7,3 @@ edin = numMain%10;
 
 result = sotni*10+edin;
 Console.WriteLine("Число, которое получено после операций: "+result);
-}
