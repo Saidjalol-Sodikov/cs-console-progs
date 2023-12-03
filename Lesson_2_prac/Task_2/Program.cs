29e4e4a15288f46bbbcf7d838450cab499ec5c1b
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-04 02:11:01 +0500

    Аdd 2-lesson's 2-task - working right

---
+++ Lesson_2_prac/Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите трёхзначное чило: ");
+int numMain = Convert.ToInt32(Console.ReadLine());
+int desyat, edin, result=1;
+
+desyat = (numMain%100-numMain%10)/10;
+edin = numMain%10;
+if (desyat == 0 && edin == 0) result = 0;
+for (int i = 1; i<=edin; i++) {
+    result=result*desyat;
+}
+Console.WriteLine(result);
