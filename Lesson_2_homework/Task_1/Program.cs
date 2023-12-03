c7795b9490470f86502f8f5dfd9d34b2f75429a3
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-04 02:58:30 +0500

    Add 2-homework's 1-task - working right

---
+++ Lesson_2_homework/Task_1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число: ");
+int numMain = Convert.ToInt32(Console.ReadLine());
+
+if (numMain%7==0 && numMain%23==0) Console.WriteLine("Число кратно и 7, и 23");
+else Console.WriteLine("Число не кратно 7 и 23");
