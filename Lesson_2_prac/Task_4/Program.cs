c8a4519d13a5c2f7206a1db2443f0d340d03c5c6
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-04 02:38:08 +0500

    Аdd 2-lesson's 4-task - working right (now)

---
+++ Lesson_2_prac/Task_4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите чиcло: ");
+int numMain = Convert.ToInt32(Console.ReadLine());
+int sotni;
+if (numMain/100==0) {
+    Console.WriteLine("Третьей цифры нет.");
+} else {
+    sotni = (numMain%1000-numMain%100)/100;
+    Console.WriteLine(sotni);
+    }
