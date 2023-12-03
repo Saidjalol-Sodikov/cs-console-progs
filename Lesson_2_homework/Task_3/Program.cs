3f3b6075b7fa391b17d045d6a0ea2ae707b78b2e
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-04 03:55:44 +0500

    Add 2-homework's 3-task - working right

---
+++ Lesson_2_homework/Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Adding part
+Console.Write("Введите число в от 10 до 99: ");
+int numMain = Convert.ToInt32(Console.ReadLine());
+
+int result = -3, desaytki, edenits;
+//Main code
+if (numMain>=10 && numMain<=99){
+    desaytki = (numMain-numMain%10)/10;
+    edenits = numMain%10;
+    if (desaytki>edenits) result=desaytki;
+    if (desaytki<edenits) result=edenits;
+    if (desaytki==edenits) result=-2;
+} 
+else {
+    result = -1;
+}
+//Show results
+if (result == -1) Console.WriteLine("Необходимо вводить число в правильном диапазоне");
+if (result >= 0)  Console.WriteLine("Большая цифра числа выше: "+result);
+if (result == -2) Console.WriteLine("Обе цифры числа выше равны");
