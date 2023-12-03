e43dec3fda39df9fdafa90cfb29003e380ae0bb6
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-04 02:26:25 +0500

    Аdd 2-lesson's 3-task - working right

---
+++ Lesson_2_prac/Task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Является ли второе число кратным первому?");
+if ((num1%num2)!=0) {
+    Console.Write("Нет, "+num1%num2);
+} else {
+    Console.Write("Да");
+}
