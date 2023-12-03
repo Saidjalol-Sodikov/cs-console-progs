97051acb7dd0672adec1f6c4b2d091d6ea29b97a
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-04 03:14:35 +0500

    Add 2-homework's 2-task - working right

---
+++ Lesson_2_homework/Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите значение X:");
+int numX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение Y:");
+int numY = Convert.ToInt32(Console.ReadLine());
+
+if (numX == 0 || numY == 0) {
+    Console. WriteLine("Вводите значения отличные от нуля");
+} 
+else {
+    if (numX > 0) {
+        if (numY > 0) {
+            Console.WriteLine("Точка находиться на координатной четверти 1");
+        } 
+        else {
+            Console.WriteLine("Точка находиться на координатной четверти 4");
+        }
+   }
+   else {
+        if (numY > 0) {
+           Console.WriteLine("Точка находиться на координатной четверти 2");
+        } 
+        else {
+            Console.WriteLine("Точка находиться на координатной четверти 3");
+        }
+   }
+}
