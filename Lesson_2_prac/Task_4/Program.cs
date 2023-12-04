baeab97eb6c7f2ce7436394918b81d4ac1edcbf5
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-05 01:40:50 +0500

    Add 2-practice's 4-task - working right

--- Lesson_2_prac/Task_4/Program.cs
+++ Lesson_2_prac/Task_4/Program.cs
@@ -2,8 +2,18 @@
 int numMain = Convert.ToInt32(Console.ReadLine());
 int sotni;
 if (numMain/100==0) {
-    Console.WriteLine("Третьей цифры нет.");
+    Console.WriteLine("Третьей цифры справа нет.");
 } else {
+    numMain = Math.Abs(numMain);
     sotni = (numMain%1000-numMain%100)/100;
-    Console.WriteLine(sotni);
+    Console.WriteLine("Третья цифры справа :"+sotni);
+    }
+if (numMain/100==0) {
+    Console.WriteLine("Третья цифры слева нет.");
+} else {
+    numMain = Math.Abs(numMain);    
+    while (numMain>=1000) {
+        numMain = numMain / 10;
+        }
+    Console.WriteLine("Третья цифры слева :"+numMain%10);
     }
