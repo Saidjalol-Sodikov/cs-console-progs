237850be4692655175a5ef90ad6d061806273365
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-05 01:22:04 +0500

    Add 2-Homework's 4-task - working right

--- Lesson_2_homework/Task_4/Program.cs
+++ Lesson_2_homework/Task_4/Program.cs
@@ -1,8 +1,14 @@
-﻿string stringMain = Console.ReadLine();
-int count = 1;
+﻿Console.Write("Введите число :");
+int numMain = Convert.ToInt32(Console.ReadLine());
 
-
-while (count<stringMain.Lenght(stringMain)) {
-    Console.WriteLine(", "+stringMain[count]);
-    count++;
+while (numMain>10) {
+    int numDec = numMain;
+    int counter = 0;
+    while (numDec>=10) {
+        numDec = numDec / 10;
+        counter++;
+    }
+    Console.Write(numDec+", ");
+    numMain = numMain - numDec*Convert.ToInt32(Math.Pow(10, counter));
 }
+Console.WriteLine(numMain);
