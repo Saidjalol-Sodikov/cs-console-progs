0280d38f8ad3a046effae8aac17d7cdf21a6319c
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-07 01:33:14 +0500

    Add 3-practice lesson's 3-task - working right

---
+++ Lesson_3_prac/Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Введение начальных данных
+Console.Write("Введите длину массива: ");
+int arreySize = Convert.ToInt32(Console.ReadLine());
+
+//Получение рабочего материала
+int[] arrey = new int[arreySize];
+Console.Write("Изначальный массив :");
+for (int i = 0; i < arreySize; i++)
+{
+    arrey[i] = new Random().Next(1, 10);
+    Console.Write(arrey[i] + " ");
+}
+Console.WriteLine();
+
+//Решение
+for (int i = 0; i < arreySize/2; i++)
+{
+    Console.Write((arrey[i] * arrey[(arreySize-1)-i]) + " "); //Выведение результатов
+}
+if(arreySize%2!=0) Console.WriteLine($"(элемент {arrey[arreySize/2]} не имеет пары)");
