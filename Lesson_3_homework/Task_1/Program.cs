a9e2baaab462cb2de6ddfea6d1db00d6df7354a7
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-07 02:08:17 +0500

    Add 3-homework's 1-task - working right

---
+++ Lesson_3_homework/Task_1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Введение начальных данных
+Console.Write("Введите длину массива: ");
+int arreySize = Convert.ToInt32(Console.ReadLine());
+
+//Получение рабочего материала
+int[] arrey = new int[arreySize];
+int result = 0;
+for (int i = 0; i < arreySize; i++)
+{
+    arrey[i] = new Random().Next(1, 100);
+    Console.Write(arrey[i] + " ");
+    
+    //Решение задачи
+    if (arrey[i] >= 20 && arrey[i] <= 90) result++;
+}
+Console.WriteLine();
+
+//Выведение результатов
+Console.WriteLine("Количество элементов от 20 до 90 включитенльно в массиве выше: "+result);
