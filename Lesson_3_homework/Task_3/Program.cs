763b00e6052ad957e46837d65a4b3ec2dee1954e
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-07 02:34:21 +0500

    Add 3-homework's 3-task - working right

---
+++ Lesson_3_homework/Task_3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Введение начальных данных
+Console.Write("Введите длину массива: ");
+int arreySize = Convert.ToInt32(Console.ReadLine());
+
+//Получение рабочего материала
+double[] arrey = new double[arreySize];
+double min = 101, max = -1, result = 0;
+for (int i = 0; i < arreySize; i++)
+{
+    arrey[i] = new Random().Next(1, 10000);
+    arrey[i] = arrey[i] / 100;
+    Console.Write(arrey[i] + " ");
+
+    //Решение задачи
+    if (arrey[i] > max ) max = arrey[i];
+    if (arrey[i] < min ) min = arrey[i];
+}
+result = max - min;
+Console.WriteLine();
+
+//Выведение результатов
+Console.WriteLine("Разница между максимальным и минимальным элементами массива выше: " + result);
