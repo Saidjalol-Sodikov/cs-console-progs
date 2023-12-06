59eff48377c7741a688bbf9f8da56da5fbf2057c
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-07 01:09:11 +0500

    Add 3-practice lesson's 2-task - working right

---
+++ Lesson_3_prac/Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Введение начальных данных
+Console.Write("Введите длину массива: ");
+int arreySize = Convert.ToInt32(Console.ReadLine());
+
+//Получение рабочего материала
+int[] arrey = new int[arreySize];
+Console.Write("Изначальный массив :");
+for (int i = 0; i < arreySize; i++)
+{
+    arrey[i] = new Random().Next(-10, 10);
+    Console.Write(arrey[i] + " ");
+}
+Console.WriteLine();
+
+//Решение задачи
+Console.Write("Отформатированный массив :");
+for (int i = 0; i < arreySize; i++)
+{
+    Console.Write((arrey[i]*(-1)) + " ");              //Выведение результатов
+}
