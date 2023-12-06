9e720253b8256993c7ca34681f227878aec3413e
Saidjalol Sodikov <ssodikovu@gmail.com>
2023-12-07 02:13:06 +0500

    Add 3-homework's 2-task - working right

---
+++ Lesson_3_homework/Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Введение начальных данных
+Console.Write("Введите длину массива: ");
+int arreySize = Convert.ToInt32(Console.ReadLine());
+
+//Получение рабочего материала
+int[] arrey = new int[arreySize];
+int result = 0;
+for (int i = 0; i < arreySize; i++)
+{
+    arrey[i] = new Random().Next(1, 100);
+    Console.Write(arrey[i] + " ");
+
+    //Решение задачи
+    if (arrey[i]%2==0) result++;
+}
+Console.WriteLine();
+
+//Выведение результатов
+Console.WriteLine("Количество чётных чисел в массиве выше: " + result);
